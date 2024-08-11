e5bab28d9ddb6424c45331d68cac36a42d38d0dc
Sy Brand <tartanllama@gmail.com>
2024-08-11 10:19:24 +0100

    Add script for cherry picking changes into later branches

---
+++ tools/update_later_branches.py
@@ -0,0 +1,14 @@
+import sys
+from git import Repo
+
+chapters = list(range(1,8))
+chapters.remove(2)
+branches = ['chapter-'+str(i) for i in chapters]
+
+repo = Repo('.')
+start_index = branches.index(repo.active_branch.name)
+commit = repo.head.object.hexsha
+
+for i in range(start_index + 1, len(branches)):
+    repo.git.checkout(branches[i])
+    repo.git.cherry_pick(commit)
