f12a9bf52fa52c893bc2e32a2fb2426a3f34ac22
Sy Brand <tartanllama@gmail.com>
2024-08-31 11:30:27 +0100

    Fix registers typo

--- include/libsdb/process.hpp
+++ include/libsdb/process.hpp
@@ -44,7 +44,7 @@ namespace sdb {
         void write_user_area(std::size_t offset, std::uint64_t data);
 
         void write_fprs(const user_fpregs_struct& fprs);
-        void write_gprs(const user_regs_struct& fprs);
+        void write_gprs(const user_regs_struct& gprs);
 
     private:
         process(pid_t pid, bool terminate_on_end, bool is_attached)
