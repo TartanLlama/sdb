45e5a4f3a9e3f232a4defc58e1c4172ba41a1561
Sy Brand <tartanllama@gmail.com>
2024-09-16 21:03:03 +0100

    Fix inlining warning

--- test/targets/step.cpp
+++ test/targets/step.cpp
@@ -1,12 +1,12 @@
 #include <cstdio>
 
 __attribute__((always_inline))
-void scratch_ears() {
+inline void scratch_ears() {
 	std::puts("Scratching ears");
 }
 
 __attribute__((always_inline))
-void pet_cat() {
+inline void pet_cat() {
 	scratch_ears();
 	std::puts("Done petting cat");
 }
